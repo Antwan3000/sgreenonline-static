2f4703a7b085af0666030c3a3236b027fcbe954c
Antwan Foster <Antwan3000@gmail.com>
2011-01-27 09:44:01 -0500

    Restructured deployment script

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,40 +1,48 @@
-default_run_options[:pty] = true  # Must be set for the password prompt from git to work
-ssh_options[:forward_agent] = true
+#############################################################
+#    Deployment Settings
+#############################################################
+set :application, "sgreenonline.com"
+set :deploy_to, "/var/www/projects/#{application}"
+set :keep_releases, 3  ## Limited number of releases stored
 set :use_sudo, false
 set :rake, "/usr/local/rvm/gems/ruby-1.9.2-p0@rails3/bin/rake"
-#set :migrate_env, "production"
+default_run_options[:pty] = true
 
-set :application, "sgreenonline.com"
+#############################################################
+#    GIT Version Control
+#############################################################
+set :scm, :git
 set :repository,  "git@github.com:Antwan3000/sgreenonline-static.git"
-set :deploy_to, "/var/www/projects/#{application}"
-
-## SCM Setup
-set :scm, :git  # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
-set :scm_verbose, true
 set :branch, "master"
 set :deploy_via, :remote_cache
 set :scm_command, "/opt/local/bin/git"
+set :scm_verbose, true
 set :local_scm_command, :default
 
-## SSH Setup
+#############################################################
+#    SSH
+#############################################################
 ssh_options[:username] = 'puma'
 ssh_options[:port] = 22
-#ssh_options[:verbose] = :debug
 ssh_options[:host_key] = 'ssh-rsa'
+ssh_options[:forward_agent] = true
 ssh_options[:keys] = %w(/home/puma/.ssh/authorized_keys)
+#ssh_options[:verbose] = :debug
 
-## Less releases, less space wasted
-set :keep_releases, 3
-
-role :web, "broadmedium.com"                          # Your HTTP server, Apache/etc
-#role :app, "broadmedium.com"                          # This may be the same as your `Web` server
-#role :db,  "broadmedium.com", :primary => true        # This is where Rails migrations will run
+#############################################################
+#    Servers
+#############################################################
+role :web, "broadmedium.com"                    # Your HTTP server, Apache/etc
+#role :app, "broadmedium.com"                    # This may be the same as your `Web` server
+#role :db,  "broadmedium.com", :primary => true  # This is where Rails migrations will run
 #role :db,  "your slave db-server here"
 
+#############################################################
+#    Passenger
+#############################################################
 # If you are using Passenger mod_rails uncomment this:
 # if you're still using the script/reapear helper you will need
 # these http://github.com/rails/irs_process_scripts
-
 # namespace :deploy do
 #   task :start do ; end
 #   task :stop do ; end
@@ -43,8 +51,10 @@ role :web, "broadmedium.com"                          # Your HTTP server, Apache
 #   end
 # end
 
-
-# Override default tasks which are not relevant to a non-rails app.
+#############################################################
+#    Override Tasks
+#    Default tasks irrelevant for static site.
+#############################################################
 namespace :deploy do
   task :migrate do
     puts "    not doing migrate because not a Rails application."
